d5148c555bac9287f9a80cc05767d4e869f18870
COMPU-S028\daw1 <agutierrezp18@educantabria.es>
2026-09-24 10:48:01 +0200

    Empezando actividad 1

---
+++ TEMA 2/Actividad01_UD2/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main() {
+        Scanner entrada = new Scanner(System.in);
+
+    //Ejercicio 1: Escribe un programa que dé los “buenos días”.
+        System.out.println("EJERCICIO  1");
+        System.out.println("BUENOS DIAS");
+
+    //Ejercicio 2: Escribe un programa que calcule y muestre el área de un cuadrado de lado igual a 5.
+        System.out.println("EJERCICIO  2");
+        int ladoCuadrado = 5;
+        int areaCuadrado = ladoCuadrado * 2;
+        System.out.println("El área del cuadrado es " + areaCuadrado);
+
+    //Ejercicio 3: Escribe un programa que calcule el área de un cuadrado cuyo lado se introduce por teclado.
+        System.out.println("EJERCICIO  3");
+        System.out.println("Introduzca el área de un cuadrado");
+
+
+
+
+    }
+}
--- primerEjemplo/src/Main.java
+++ primerEjemplo/src/Main.java
@@ -1,8 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println("HOLA MUNDO!");
 
+public class Main {
+
+    public static void main() {
+
+        //Imprimir por pantalla
+        IO.println("HOLA MUNDO!");
+        System.out.println("HOLA MUNDO");
+
+        int lado = 5;
+        int areaLado = lado * 2;
+        System.out.println("El área del lado es " + areaLado);
+
+
+
+    }
 }
